d3a948ac0d7c9620227f5bd023534a87b311d16d
fsbakhtiar <farzadsbakhtiar@gmail.com>
2024-02-12 11:58:56 -0500

    added mistral model

--- rag_test.py
+++ rag_test.py
@@ -2,13 +2,13 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-from langchain.llms import HuggingFacePipeline
 from langchain.docstore.document import Document
-from langchain.llms import LlamaCpp
-from langchain.embeddings import LlamaCppEmbeddings
 
-CACHE_DIR = './cache'
-CHROMA_DIR = './chroma'
+from utils import CACHE_DIR, CHROMA_DIR,Models
+
+# MODEL = Models.FLAN_LARGE
+MODEL = Models.MISTRAL
+
 # document = 'the first part of the name of this laptop is john' * 100 + 'the second part is atkins' * 100
 document = 'the name of the laptop is john doe'
 
@@ -30,28 +30,10 @@ retriever = chromadb_index.as_retriever()
 
 chain_type = "stuff"  # Options: stuff, map_reduce, refine, map_rerank
 laptop_qa = RetrievalQA.from_chain_type(
-    llm=hf_llm, chain_type="stuff", retriever=retriever
+    llm=Models.get_llm(MODEL), chain_type="stuff", retriever=retriever
 )
 
 laptop_name = laptop_qa.run("What is the full name of the laptop?") # str
 
 print(laptop_name)
 
-def get_embed_and_llm():
-    if MODEL == 'google':
-        model = HuggingFacePipeline.from_model_id(
-            model_id="google/flan-t5-large",
-            task="text2text-generation",
-            model_kwargs={
-                "temperature": 0,
-                "max_length": 128,
-                "cache_dir": CACHE_DIR,
-            },
-        )
-    elif MODEL == 'astral':
-        # model code from:
-        # https://huggingface.co/TheBloke/MPT-7B-Instruct-GGML/discussions/2
-        model_id = "TheBloke/Mistral-7B-OpenOrca-GGUF",
-        llm = LlamaCpp
-        model = HuggingFacePipeline(pipeline=pipe)
-        embeddings = LlamaCppEmbeddings(model_path="models/llama-7b.ggmlv3.q4_0.bin")
---
+++ utils.py
@@ -0,0 +1,38 @@
+import enum
+
+from langchain.llms import HuggingFacePipeline
+from ctransformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline
+
+
+
+CACHE_DIR = './cache'
+CHROMA_DIR = './chroma'
+
+
+class Models:
+    FLAN_LARGE = "google/flan-t5-large" # 800M
+    MISTRAL = "TheBloke/Mistral-7B-OpenOrca-GGUF" # 7B
+    @staticmethod
+    def get_llm(model: str):
+        if model == Models.FLAN_LARGE:
+            model = HuggingFacePipeline.from_model_id(
+                model_id=model,
+                task="text2text-generation",
+                model_kwargs={
+                    "temperature": 0,
+                    "max_length": 128,
+                    "cache_dir": CACHE_DIR,
+                },
+            )
+            return model
+        elif model == 'astral':
+            # for more inspiration look at:
+            # https://huggingface.co/TheBloke/MPT-7B-Instruct-GGML/discussions/2
+            model_id = model
+            llm = AutoModelForCausalLM.from_pretrained(model_id, hf=True)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
+            pipe = pipeline("text2text-generation", model=llm, tokenizer=tokenizer)
+            return pipe
+            # let's just use the regular embeddings for now
+            # embeddings = LlamaCppEmbeddings(model_path="models/llama-7b.ggmlv3.q4_0.bin")
