76424c6949c4149c710fd989c22c193bb020f40d
fsbakhtiar <farzadsbakhtiar@gmail.com>
2024-02-20 16:47:39 -0500

    testing other document configurations

--- rag_test.py
+++ rag_test.py
@@ -7,14 +7,18 @@ from langchain.docstore.document import Document
 from utils import CACHE_DIR, CHROMA_DIR,Models
 
 # MODEL = Models.FLAN_LARGE
-MODEL = Models.FLAN_LARGE
+MODEL = Models.MISTRAL
 
 # document = 'the first part of the name of this laptop is john' * 100 + 'the second part is atkins' * 100
-document = 'the name of the laptop is john doe'
-
+# document = 'the name of the laptop is john doe'
+documents = [
+    'the first name of the laptop is john',
+    'the last name of the laptop is doe',
+    'the full brand of the laptop is macbook buddy',
+]
 # First we split the data into manageable chunks to store as vectors. There isn't an exact way to do this, more chunks means more detailed context, but will increase the size of our vectorstore.
 text_splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=10)
-texts = text_splitter.split_documents([Document(document, metadata={'desc': 'first_test_doc'})])
+texts = text_splitter.split_documents([Document(d) for d in documents])
 # Now we'll create embeddings for our document so we can store it in a vector store and feed the data into an LLM. We'll use the sentence-transformers model for out embeddings. https://www.sbert.net/docs/pretrained_models.html#sentence-embedding-models/
 model_name = "sentence-transformers/all-MiniLM-L6-v2"
 embeddings = HuggingFaceEmbeddings(
@@ -27,6 +31,7 @@ chromadb_index = Chroma.from_documents(
 
 retriever = chromadb_index.as_retriever()
 
+print('******chroma done*****')
 
 chain_type = "stuff"  # Options: stuff, map_reduce, refine, map_rerank
 laptop_qa = RetrievalQA.from_chain_type(
