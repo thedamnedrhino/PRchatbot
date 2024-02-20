b2ae130f11de06e88b6e9d9ff8ee7ff4aa5b6948
fsbakhtiar <farzadsbakhtiar@gmail.com>
2024-02-20 17:21:55 -0500

    extracted the retriever into a separate module

--- rag_test.py
+++ rag_test.py
@@ -1,35 +1,10 @@
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-from langchain.docstore.document import Document
 
 from utils import CACHE_DIR, CHROMA_DIR,Models
 
 # MODEL = Models.FLAN_LARGE
 MODEL = Models.MISTRAL
 
-# document = 'the first part of the name of this laptop is john' * 100 + 'the second part is atkins' * 100
-# document = 'the name of the laptop is john doe'
-documents = [
-    'the first name of the laptop is john',
-    'the last name of the laptop is doe',
-    'the full brand of the laptop is macbook buddy',
-]
-# First we split the data into manageable chunks to store as vectors. There isn't an exact way to do this, more chunks means more detailed context, but will increase the size of our vectorstore.
-text_splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=10)
-texts = text_splitter.split_documents([Document(d) for d in documents])
-# Now we'll create embeddings for our document so we can store it in a vector store and feed the data into an LLM. We'll use the sentence-transformers model for out embeddings. https://www.sbert.net/docs/pretrained_models.html#sentence-embedding-models/
-model_name = "sentence-transformers/all-MiniLM-L6-v2"
-embeddings = HuggingFaceEmbeddings(
-    model_name=model_name, cache_folder=CACHE_DIR
-)  # Use a pre-cached model
-# Finally we make our Index using chromadb and the embeddings LLM
-chromadb_index = Chroma.from_documents(
-    texts, embeddings, # persist_directory=CHROMA_DIR, -- this is the persistent version
-)
-
-retriever = chromadb_index.as_retriever()
 
 print('******chroma done*****')
 
---
+++ retriever.py
@@ -0,0 +1,65 @@
+import json
+
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.docstore.document import Document
+
+from utils import CACHE_DIR
+
+test_documents = [
+    'the first name of the laptop is john',
+    'the last name of the laptop is doe',
+    'the full brand of the laptop is macbook buddy',
+]
+
+
+def get_retriever(documents: list[str] = None):
+    text_splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=10)
+    texts = text_splitter.split_documents([Document(d) for d in documents])
+    # Now we'll create embeddings for our document so we can store it in a vector store and feed the data into an LLM. We'll use the sentence-transformers model for out embeddings. https://www.sbert.net/docs/pretrained_models.html#sentence-embedding-models/
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name, cache_folder=CACHE_DIR
+    )  # Use a pre-cached model
+    # Finally we make our Index using chromadb and the embeddings LLM
+    chromadb_index = Chroma.from_documents(
+        texts, embeddings,
+        # persist_directory=CHROMA_DIR, -- this is the persistent version
+    )
+
+    retriever = chromadb_index.as_retriever()
+
+    return retriever
+
+
+def get_issues():
+    issues = get_issues_json()
+    documents = []
+    for issue in issues:
+        # -- just search the issues by title
+        documents.append(Document(issue['title'],
+                                  # -- give the full text to the llm
+                                  metadata={'formatted': format_issue(issue),
+                                            'item_type': 'ISSUE'}))
+    return documents
+
+
+def get_issues_json() -> list[dict]:
+    with open('issues.json', 'r') as file:
+        docs = json.load(file)
+    return docs
+
+
+def format_issue(issue: dict) -> str:
+    """
+    Converts an issue in dict format to an str for the LLM.
+    """
+    return f"""Item type: ISSUE
+Number: #{issue['number']}
+State: {issue['state']}, 
+Labels: {', '.join([l['name'] for l in issue['labels']])},
+ 
+Title: {issue['title']}
+Body: {issue['body']}
+"""
